2bec3c1490ed8d183c1ecde3c32adf7b656c86b1
taek <spvpt44@naver.com>
2023-02-16 14:19:00 +0900

    2023-02-16 김성택 - 브랜치 커밋 풀 테스트

--- SK/src/main/java/sk/mypage/controller/MyPageController.java
+++ SK/src/main/java/sk/mypage/controller/MyPageController.java
@@ -2,7 +2,8 @@ package sk.mypage.controller;
 
 import java.util.Map;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,19 +12,19 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MyPageController {
 
-	Logger log = Logger.getLogger(this.getClass());
-	
-	@GetMapping(value="/myPage")
+	Logger log = LoggerFactory.getLogger("testLogger");
+
+	@GetMapping(value = "/myPage")
 	public ModelAndView myPageMain(@RequestParam Map<String, Object> map) throws Exception {
 		ModelAndView mv = new ModelAndView("myPageMain");
-	 
-	 return mv;
+
+		return mv;
 	}
-	
-	@GetMapping(value="/accountModifyForm")
+
+	@GetMapping(value = "/accountModifyForm")
 	public ModelAndView accountModifyForm(@RequestParam Map<String, Object> map) throws Exception {
 		ModelAndView mv = new ModelAndView("accountModifyForm");
-	 
-	 return mv;
+
+		return mv;
 	}
 }
