4a316baa029a2f88ebe58d3f08cd0de7e914cbc9
taek <spvpt44@naver.com>
2023-02-22 12:39:42 +0900

    2023-02-22 성택 - 민준branch marge

--- SK/src/main/java/sk/reservation/controller/ReservationController.java
+++ SK/src/main/java/sk/reservation/controller/ReservationController.java
@@ -2,8 +2,11 @@ package sk.reservation.controller;
 
 import java.util.Map;
 
+<<<<<<< HEAD
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+=======
+>>>>>>> parent of b4f038a (2023-02-21 성택 - 로그 설정 중(쿼리 제외 정상 작동))
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,21 +15,26 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ReservationController {
 
+<<<<<<< HEAD
 	Log log = LogFactory.getLog(this.getClass());
 
 	// reservationList
 	@GetMapping(value = "/reservationList")
+=======
+	//reservationList
+	@GetMapping(value="/reservationList")
+>>>>>>> parent of b4f038a (2023-02-21 성택 - 로그 설정 중(쿼리 제외 정상 작동))
 	public ModelAndView reservationList(@RequestParam Map<String, Object> map) throws Exception {
 		ModelAndView mv = new ModelAndView("reservationList");
-
-		return mv;
+	 
+	 return mv;
 	}
-
-	// reservationDetail
-	@GetMapping(value = "/reservationDetail")
+	
+	//reservationDetail
+	@GetMapping(value="/reservationDetail")
 	public ModelAndView reservationDetail(@RequestParam Map<String, Object> map) throws Exception {
 		ModelAndView mv = new ModelAndView("reservationDetail");
-
-		return mv;
+	 
+	 return mv;
 	}
 }
