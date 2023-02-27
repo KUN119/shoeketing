095c34b0e575fcf376eb4e7ebd77f806d45c0eac
Yunjeong93 <108472295+Yunjeong93@users.noreply.github.com>
2023-02-27 23:23:49 +0900

    2023-02-27 윤정 - 매장 재고관리 구현 완료, 픽업 예약 구현(미완성)

---
+++ SK/src/main/java/sk/item/service/ReservationService.java
@@ -0,0 +1,42 @@
+package sk.item.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ReservationService {
+
+	//  나의 픽업 예약 리스트 (일반회원)
+	public List<Map<String, Object>> selectPickupList(Map<String, Object> map) throws Exception;
+	
+	// 픽업 예약 리스트 토탈개수 (일반회원)
+	public int selectPickupCount(Map<String, Object> map) throws Exception;
+	
+	// 픽업 예약 상세조회
+	public Map<String, Object> selectPickupDetail(Map<String, Object> map) throws Exception;
+	
+	// 예약 취소. 일반 회원/매장이 예약 대기중인 상태에서 예약 취소 혹은, 매장이 픽업 승인 거부. 이때는 재고변동 X
+	public int deleteReservation(Map<String, Object> map) throws Exception;
+	
+	// 픽업 대기중. 일반 회원/매장이 픽업 대기중인 상태에서 픽업 예약 취소. 이때는 재고변동 O
+	public int deletePickup(Map<String, Object> map) throws Exception;
+	
+	// 픽업 취소 후 재고 +1
+	public int updateStockIncrease(Map<String, Object> map) throws Exception;
+	
+	// 픽업 예약 요청 리스트 (매장)
+	public List<Map<String, Object>> selectReservationRequestList(Map<String, Object> map) throws Exception;
+	
+	// 해당매장의 총 픽업 예약개수 (매장)
+	public int selectReservationCount(Map<String, Object> map) throws Exception;
+	
+	// 픽업 예약 승인 (매장)
+	public int updateReservationApprove(Map<String, Object> map) throws Exception;
+	
+	// 픽업 예약 승인시, 재고 -1 (매장)
+	public int updateStockDecrease(Map<String, Object> map) throws Exception;
+	
+	// 예약금 결제 성공
+	public int insertReservation(Map<String, Object> map) throws Exception; 
+		
+	
+}
