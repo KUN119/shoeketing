0bb65b66edd43a4b0581930400d2ce18dca3d357
Yunjeong93 <108472295+Yunjeong93@users.noreply.github.com>
2023-03-05 21:50:52 +0900

    2023-03-05 윤정 - 재고검색폼 로직 일부 수정

--- SK/src/main/java/sk/item/controller/StockController.java
+++ SK/src/main/java/sk/item/controller/StockController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import sk.common.service.CommonService;
 import sk.item.service.StockService;
 
 @RestController
@@ -23,6 +25,9 @@ public class StockController {
 
 	@Resource(name = "stockService")
 	private StockService stockService;
+	
+	@Resource(name = "sessionService")
+	private CommonService sessionService;
 
 	@GetMapping(value = "/shopPage/stockList")
 	public ModelAndView shopStockList(Map<String, Object> map) throws Exception {
@@ -51,9 +56,17 @@ public class StockController {
 	}
 
 	@GetMapping(value = "/goods/stockSearchForm")
-	public ModelAndView stockSearchForm(@RequestParam Map<String, Object> map) throws Exception {
+	public ModelAndView stockSearchForm(@RequestParam Map<String, Object> map, HttpSession session) throws Exception {
 		log.debug("###### 실시간 재고 검색폼 ######");
 		ModelAndView mv = new ModelAndView("stockSearchForm");
+		
+		// 로그인 한 회원 정보 세션에서 가져오기
+		String memName = sessionService.getSession(session, "MEM_NAME");
+		String memEmail = (String) session.getAttribute("session_MEM_ID");
+		
+		System.out.println("이름 : " + sessionService.getSession(session, "MEM_NAME"));
+		System.out.println("이름 : " + (String) session.getAttribute("session_MEM_ID"));
+		
 		return mv;
 	}
 
