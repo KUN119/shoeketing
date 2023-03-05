d02ae9a3144cc73563674c4e19a9ad8e8774232b
KUN119 <124117010+KUN119@users.noreply.github.com>
2023-03-05 14:02:37 +0900

    23-03-05 건주:noticelist, findid(미완), findpw(미완) sweetalert SDK추가(에러)

--- SK/src/main/java/sk/user/service/MailService.java
+++ SK/src/main/java/sk/user/service/MailService.java
@@ -1,10 +1,12 @@
 package sk.user.service;
 
+import java.util.Map;
+
 public interface MailService {
 
-	public void makeRandomNumber();
+//	public void makeRandomNumber();
 
-	public String joinEmail(String email);
+	public String joinEmail(Map<String, Object> map) throws Exception;
 
 	public void mailSend(String setFrom, String toMail, String title, String content);
 
